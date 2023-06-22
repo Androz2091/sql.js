b001c816273c6bebaed05fe18cc0548cb3a93bbc
Androz2091 <androz2091@gmail.com>
2023-06-22 09:09:39 +0200

    add generate series test

--- test/test_generate_series.js
+++ test/test_generate_series.js
@@ -6,7 +6,7 @@ exports.test = function(sql, assert){
     //db.run('SELECT _sqlite3_series_init();');
 
     var result = db.exec("SELECT * FROM generate_series(1,5);");
-    assert.deepEqual(result, [{columns:['generate_series'], values:[[1],[2],[3],[4],[5]]}], "generate_series(1,5)");
+    assert.deepEqual(result, [{columns:['value'], values:[[1],[2],[3],[4],[5]]}], "generate_series(1,5)");
 
     // Close the database and all associated statements
     db.close();
